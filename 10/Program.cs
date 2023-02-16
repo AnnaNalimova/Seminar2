d5a51909b7fcaae17cc62d3fd5e95a861195f575
Anna Nalimova <avnalimova@mail.ru>
2023-02-16 18:40:53 +0300

    10

---
+++ 10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine ("Введите трехзначное число ");
+int numberA = int.Parse(Console.ReadLine());
+int x1 = numberA%10;
+int x2 = numberA/10;
+int y = x2%10;
+Console.WriteLine($"Вторая цифра {numberA} -> {y}");
